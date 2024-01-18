8c8e2c2cb7bbbf53739a4926a766e7c2c49a0169
Taj Sharma <sharma.tajinder01@gmail.com>
2024-01-18 13:11:23 -0800

    import sql tools and instantiate a basedirectory path to bind with db

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,10 +1,23 @@
-from flask import Flask
+import os
+from flask import Flask, render_template, request, url_for, redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.sql import func
 
+#set the base directory to one this file is contained in
+basedir = os.path.abspath(os.path.dirname(__file__))
+#initiate the app
 app =Flask(__name__)
+
+#combine the path to our basedirectory and our database 
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+    'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
 
 @app.route("/members")
 def members():
-    return{"members": ["m1","m2","m3"]}
+    return{"members": ["lebron","m2","m3"]}
 
 if __name__ == "__main__":
     app.run(debug=True)
