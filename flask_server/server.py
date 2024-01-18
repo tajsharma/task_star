307401924a3d109105027e34c5577128fa9193a0
Taj Sharma <sharma.tajinder01@gmail.com>
2024-01-18 13:24:50 -0800

    add task skeleton to the server file, import datetime

--- flask_server/server.py
+++ flask_server/server.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
-from tasks import db, Task
+from datetime import datetime
 
 #set the base directory to one this file is contained in
 basedir = os.path.abspath(os.path.dirname(__file__))
@@ -16,9 +16,19 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
-@app.route("/members")
-def members():
-    return{"members": ["lebron","m2","m3"]}
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    taskName = db.Column(db.String(100), nullable=False)
+    timeStart = db.Column(db.DateTime, default=datetime.utcnow)
+    timeSpent = db.Column(db.Integer, default=0)  # Time in minutes
+    active = db.Column(db.Boolean, default=False)
+    description = db.Column(db.Text, nullable=True)
+
+    def __repr__(self):
+        return f"<Task {self.taskName}>"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
